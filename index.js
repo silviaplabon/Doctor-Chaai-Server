90febe0e1ecce6ae921c29ddc19363d68a42aa5d
silviaplabon <silviaplabon@gmail.com>
2021-08-03 20:29:48 +0600

    new

--- index.js
+++ index.js
@@ -13,22 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //define port
-const port = process.env.PORT || 4200;
-const ObjectID = require('mongodb').ObjectID;
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcsxh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const AppointmentsCollection = client.db('Doctors_Chaai').collection("PatientsAppointmentsCollection");
-  const AllDoctorsCollection = client.db('Doctors_Chaai').collection("PatientsAppointmentsCollection");
+const port = 3000;
 
-
-  app.get('/allDoctorsCollection/Show', (req, res) => {
-    AllDoctorsCollection.find({})
-      .toArray((err, products) => {
-        res.send(products)
-      })
-  })
 
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cau0x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
@@ -38,7 +24,7 @@ mongoose
   .then(() => {
     console.log(`connection successful with Database`);
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err,"silvia"));
 
 
 // for doctor routes
